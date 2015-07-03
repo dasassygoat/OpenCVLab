60141bb2a719e5523c8b3f39303fe9de28e7152b
Bryan Pritchett <bryan@anastasisdev.com>
2015-07-02 23:36:30 -0400

    basically works

--- OpenCVLab/Program.cs
+++ OpenCVLab/Program.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Emgu.CV.OCR;
 using Emgu.CV;
+using Emgu.CV.Structure;
 
 namespace OpenCVLab
 {
@@ -12,6 +14,14 @@ namespace OpenCVLab
     {
         static void Main(string[] args)
         {
+            using (var image = new Image<Bgr, byte>(Path.GetFullPath("text.png")))
+            {
+                using (var tess = new Tesseract("", "eng", OcrEngineMode.TesseractCubeCombined))
+                {
+                    tess.Recognize(image);
+                    var text = tess.GetText().TrimEnd();
+                }
+            }
         }
     }
 }
